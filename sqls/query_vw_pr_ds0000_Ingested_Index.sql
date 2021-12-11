5697a2e2004ff84a2e455a10297eb1a1faad658f
Benj Carpena <benjcarpena@Helios.local>
2021-12-10 18:19:32 -0600

    20211210_181932

--- sqls/query_vw_pr_ds0000_Ingested_Index.sql
+++ sqls/query_vw_pr_ds0000_Ingested_Index.sql
@@ -1,3 +1,7 @@
 	--1	Count all records ingested
 	SELECT FORMAT(SUM(CAST(REPLACE(rows, ',', '') as int)), 'N0') + ' rows' as Record_Count 
     From dbo.vw_pr_ds0000_Ingested_Index
+
+
+	--2 Display all ingested data with row counts
+	SELECT * From dbo.vw_pr_ds0000_Ingested_Index
